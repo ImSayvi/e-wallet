c1b263019959cd0bda748c95abd0424ad9baddde
Say.vi <say.julia.vi@gmail.com>
2024-02-29 21:25:29 +0100

    function of adding income to list with icons and proper class

--- components/addTransaction.js
+++ components/addTransaction.js
@@ -2,9 +2,37 @@ const addTransactionButton = document.getElementById('addT')
 const newTransactionSection = document.getElementById('newTransaction')
 const saveButton = document.getElementById('save')
 const cancelButton = document.getElementById('cancel')
-const nameTransaction = document.getElementById('nameInput')
-const amountTransaction = document.getElementById('amountInput')
+
 const category = document.getElementById('categoryInput')
+
+
+
+saveButton.addEventListener('click', () => {
+	const nameTransaction = document.getElementById('nameInput').value
+	const amount = document.getElementById('amountInput').value;
+	const incomeList = document.createElement('li')
+	
+	if (category.value == 'income') {
+		const greenList = document.getElementById('greenList');
+		const nameSpan = document.createElement('span');
+        nameSpan.className = 'name';
+        nameSpan.innerHTML = `<i class="fa-solid fa-money-bill-1-wave"></i>${nameTransaction}`;
+
+        const incomeAmountSpan = document.createElement('span');
+        incomeAmountSpan.className = 'incomeAmount';
+        incomeAmountSpan.innerHTML = `${amount} zł<i class="fa-solid fa-xmark x"></i>`;
+
+        
+        incomeList.appendChild(nameSpan);
+        incomeList.appendChild(incomeAmountSpan);
+
+        
+        greenList.appendChild(incomeList);
+
+        console.log(greenList);
+		console.log(incomeList);
+	}
+})
 
 addTransactionButton.addEventListener('click', () => {
 	if ((newTransactionSection.style.display = 'none')) {
